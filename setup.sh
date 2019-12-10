957978df8d5dd0948762d3a0a46bd636522ec23b
tanukee <tanukee@hpcnt.com>
2019-12-10 19:54:04 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# install Rust
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# install fd
+cargo install --git https://github.com/sharkdp/fd
+
+# install bat
+cargo install --git https://github.com/sharkdp/bat
+## BAT_THEME=ansi_light # for terminal whose background is white
+
+# install exa
+cargo install --git https://github.com/ogham/exa
+
+
