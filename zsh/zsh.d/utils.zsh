f17f03e430edf29b56d0898b3b95906fd40794ed
gunjunlee <gunjunlee97@gmail.com>
2024-09-24 15:06:34 +0000

    minor change

--- zsh/zsh.d/utils.zsh
+++ zsh/zsh.d/utils.zsh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
 # define discord alert utility function
-dm() {
+dmi() {
     # check if webhook url is set
     if [ -z "$DISCORD_WEBHOOK_URL" ]; then
       echo "DISCORD_WEBHOOK_URL is not set"
       return
     fi
 
-    # check if user id is set
-    if [ -z "$DISCORD_USER_ID" ]; then
-      echo "DISCORD_USER_ID is not set"
-      return
-    fi
-
     local message="$1"
 
     # create full message
-    local full_message="<@$DISCORD_USER_ID> $message"
+    local full_message="$message"
 
     # JSON payload
     local payload=$(cat <<EOF
@@ -31,4 +25,15 @@ EOF
     curl -H "Content-Type: application/json" \
          -d "$payload" \
         "$DISCORD_WEBHOOK_URL"
+}
+
+dm() {
+    # check if user id is set
+    if [ -z "$DISCORD_USER_ID" ]; then
+      echo "DISCORD_USER_ID is not set"
+      return
+    fi
+
+    local message="<@$DISCORD_USER_ID> $1"
+    dmi "$message"
 }
