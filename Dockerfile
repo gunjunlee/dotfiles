e42824b3d6536270968ef444bf66021812767379
gunjunlee <gunjunlee97@gmail.com>
2024-10-13 17:54:48 +0000

    fix docker

--- Dockerfile
+++ Dockerfile
@@ -7,15 +7,6 @@ RUN \
     apt-get install -y software-properties-common && \
     apt-get install -y build-essential cmake ninja-build git sudo
 
-
-
-WORKDIR /home/temp
-RUN git clone https://github.com/gunjunlee/dotfiles.git ~/.dotfiles && \
-        cd ~/.dotfiles && \
-        git submodule update --init --recursive && \
-        sudo bash setup.sh && \
-        python3 install.py
-
 ARG USER_ID=1000
 ARG GROUP_ID=1000
 ARG USER_NAME=gunjunlee
@@ -24,13 +15,21 @@ RUN echo "USER_ID: ${USER_ID}"
 RUN echo "GROUP_ID: ${GROUP_ID}"
 RUN echo "USER_NAME: ${USER_NAME}"
 
-USER ${USER_NAME}
+WORKDIR /home/${USER_NAME}
 
 RUN groupadd -g ${GROUP_ID} ${USER_NAME} && \
-    useradd -m -u ${USER_ID} -g ${GROUP_ID} ${USER_NAME} && \
+    useradd -m -u ${USER_ID} -g ${GROUP_ID} ${USER_NAME} -s /bin/zsh && \
     echo "${USER_NAME} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
 RUN chown -chR ${USER_NAME}:${USER_NAME} /workspace
 RUN chown -chR ${USER_NAME}:${USER_NAME} /home/${USER_NAME}
+
+USER ${USER_NAME}
+
+RUN git clone https://github.com/gunjunlee/dotfiles.git ~/.dotfiles && \
+        cd ~/.dotfiles && \
+        git submodule update --init --recursive && \
+        sudo bash setup.sh && \
+        python3 install.py
 
 SHELL ["/bin/zsh", "-c"]
--- script.py
+++ script.py
@@ -2,64 +2,71 @@
 
 import os
 import subprocess
+import argparse
 import sys
 import termios
 import tty
 
-def select_options(options):
+CUR_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def select_options(options, desc="Select options"):
     selected = [True] * len(options)
     index = 0
 
+    num_printed_lines = 0
+
+    def _print(*args, **kwargs):
+        nonlocal num_printed_lines
+        num_printed_lines += 1
+        print(*args, **kwargs)
+
     while True:
-        os.system('clear')
-        print("\nSelect options (use arrow keys to navigate, space/enter to select, 'q' to finish, 'a' to add new custom option, Ctrl+C to exit)")
+        # Clear the screen
+        print(f"\033[F" * num_printed_lines, end="")
+        num_printed_lines = 0
+        _print(desc)
+        _print("Select options (use arrow keys to navigate, space/enter to select, 'q' to finish, 'a' to add new custom option, Ctrl+C to exit):")
         for idx, option in enumerate(options):
             prefix = "[x]" if selected[idx] else "[ ]"
             if idx == index:
-                print(f"> {prefix} {option}")
+                _print(f"> {prefix} {option}")
             else:
-                print(f"  {prefix} {option}")
+                _print(f"  {prefix} {option}")
+        _print()
 
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
         try:
             tty.setraw(sys.stdin.fileno())
             key = sys.stdin.read(1)
-
-            if key == "q":
-                break
-            elif key == "\x1b":  # Arrow keys
-                key += sys.stdin.read(2)
-                if key == "\x1b[A":  # Up arrow
-                    index = (index - 1) % len(options)
-                elif key == "\x1b[B":  # Down arrow
-                    index = (index + 1) % len(options)
-            elif key == " " or key == "\n" or key == "\r":
-                selected[index] = not selected[index]
-            elif key == "a":
-                new_option = input("Enter new option: ")
-                options.append(new_option)
-                selected.append(True)
-            # Ctrl+C to break out of the loop
-            elif key == "\x03":
-                exit(1)
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+        if key == "q":
+            break
+        elif key == "\x1b":  # Arrow keys
+            key += sys.stdin.read(2)
+            if key == "\x1b[A":  # Up arrow
+                index = (index - 1) % len(options)
+            elif key == "\x1b[B":  # Down arrow
+                index = (index + 1) % len(options)
+        elif key == " " or key == "\n" or key == "\r":
+            selected[index] = not selected[index]
+        elif key == "a":
+            guide = "Enter new option: "
+            new_option = input(guide)
+            print("\033[F" + "\b" * (len(guide) + len(new_option) + 1) + " " * (len(guide) + len(new_option) + 1), end="")
+            options.append(new_option)
+            selected.append(True)
+        # Ctrl+C to break out of the loop
+        elif key == "\x03":
+            exit(1)
 
     selected_options = [option for i, option in enumerate(options) if selected[i]]
     return selected_options
 
 def dn(container_name=None, image_name=None):
-    default_opts = [
-        f"-v {os.path.expanduser('~')}/conda:{os.path.expanduser('~')}/conda",
-        "-v /:/host",
-        "--gpus all",
-        f"-v {os.path.expanduser('~')}/.cache:{os.path.expanduser('~')}/.cache"
-    ]
-
-    selected_opts = select_options(default_opts)
-    print("Selected options:", " ".join(selected_opts))
-
     if not container_name:
         print("Usage: dn <container_name> <image_name>")
         return
@@ -71,13 +78,23 @@ def dn(container_name=None, image_name=None):
     group_id = subprocess.getoutput("id -g")
     user_name = subprocess.getoutput("whoami")
 
+    default_opts = [
+        f"-v {os.path.expanduser('~')}/conda:{os.path.expanduser('~')}/conda",
+        "-v /:/host",
+        "--gpus all",
+        f"-v {os.path.expanduser('~')}/.cache:{os.path.expanduser('~')}/.cache"
+    ]
+
+    selected_opts = select_options(default_opts, desc=f"Select options for container {container_name} (image: {image_name})")
+    print("Selected options:", " ".join(selected_opts))
+
     if not image_name:
         image_name = f"{os.getlogin()}-default"
-        # Check if image exists. If not, build it from $HOME/.dotfiles/Dockerfile
+        # Check if image exists. If not, build it from ${CUR_DIR}/Dockerfile
         if not subprocess.getoutput(f"{sudo} docker images -q {image_name}"):
             print(f"Image {image_name} does not exist")
-            print(f"Building image {image_name} from $HOME/.dotfiles/Dockerfile")
-            cmd = f"{sudo} docker build -t {image_name} --build-arg USER_ID={user_id} --build-arg GROUP_ID={group_id} --build-arg USER_NAME={user_name} -f $HOME/.dotfiles/Dockerfile"
+            print(f"Building image {image_name} from {CUR_DIR}/Dockerfile")
+            cmd = f"{sudo} docker build -t {image_name} --build-arg USER_ID={user_id} --build-arg GROUP_ID={group_id} --build-arg USER_NAME={user_name} -f {CUR_DIR}/Dockerfile"
             subprocess.run(cmd, shell=True)
 
     # Check if container is running
@@ -117,19 +134,24 @@ def da(container_name):
         print(f"Container {container_name} does not exist")
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <command> [<args>...]")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="A script with multiple commands.")
+    subparsers = parser.add_subparsers(dest="command", help="Subcommands")
 
-    command = sys.argv[1]
+    # 'dn' command
+    parser_dn = subparsers.add_parser('dn', help="Run 'dn (= docker create)' command")
+    parser_dn.add_argument('container_name', nargs='?', help="Container name")
+    parser_dn.add_argument('image_name', nargs='?', help="Image name")
 
-    if command == "dn":
-        container_name = sys.argv[2] if len(sys.argv) > 2 else None
-        image_name = sys.argv[3] if len(sys.argv) > 3 else None
-        dn(container_name, image_name)
-    elif command == "da":
-        container_name = sys.argv[2] if len(sys.argv) > 2 else None
-        da(container_name)
+    # 'da' command
+    parser_da = subparsers.add_parser('da', help="Run 'da (= docker activate)' command")
+    parser_da.add_argument('container_name', nargs='?', help="Container name")
+
+    args = parser.parse_args()
+
+    if args.command == "dn":
+        dn(args.container_name, args.image_name)
+    elif args.command == "da":
+        da(args.container_name)
     else:
-        print(f"Unknown command: {command}")
+        parser.print_help()
         sys.exit(1)
